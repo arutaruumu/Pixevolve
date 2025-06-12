b4905909211f5d9ddf18cb183ce46b143a000f60
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2025-06-12 09:51:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,23 @@ import FooterSection from "@/components/footer";
 export default function Home() {
   return (
     <>
+      {/* GA4 Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0DW35XP20Q`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0DW35XP20Q');
+          `,
+        }}
+      />
       <HeroSection />
       <Features />
       <ContentSection />
